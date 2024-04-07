5ef4430d748ac99c828bdf239bff95425d031213
ne_srv <ne_srv@mail.ru>
2024-04-07 13:20:29 +0300

    autosave

--- 24/68/68.py
+++ 24/68/68.py
@@ -4,8 +4,8 @@ mc = 0
 for i in range(len(f)):
     if f[i] in 'XY':
         s.append((f[i], i))
-       
-for x in range(len(s) - 2):   
+
+for x in range(len(s) - 2):
     if s[x][0] != s[x+1][0]:
-        mc = max(mc, s[x+2][1] - s[x-1][1] - 1) # длина [- А - В -]
+        mc = max(mc, s[x+2][1] - s[x-1][1] - 1)  # длина [- А - В -]
 print(mc)
