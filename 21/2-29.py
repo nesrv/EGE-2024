565fcbc941e9756cf10b653defd64612bc1a08d1
ne_srv <ne_srv@mail.ru>
2024-04-15 22:03:45 +0300

    autosave

--- 21/2-29.py
+++ 21/2-29.py
@@ -1,9 +1,9 @@
-#29 Тип 19 № 52190 https://inf-ege.sdamgia.ru/problem?id=52190
+# №29 Тип 21 № 52191 https://inf-ege.sdamgia.ru/problem?id=52191
 
 
 def f(x, y, n):
-    if x + y > 60 or n > 2:
-        return n == 2
+    if x + y >= 61 or n > 4:
+        return n == 4 or n == 2
     if n % 2 != 0:
         if x > y:
             return f(x, y + 1, n + 1) or f(x, y + 2, n + 1) or f(x, y * 2, n + 1)
@@ -19,4 +19,5 @@ def f(x, y, n):
 for x in range(1, 53):
     if f(8, x, 0):
         print(x)
-        break
+        
+        
