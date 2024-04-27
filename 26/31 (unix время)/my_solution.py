ac92b834555a31e23ba36c83d3cefc588c7c192b
ne_srv <ne_srv@mail.ru>
2024-04-27 23:49:47 +0300

    autosave

---
+++ 26/31 (unix время)/my_solution.py
@@ -0,0 +1,33 @@
+f = open('26/31 (unix время)/26.txt')
+
+n = f.readline()
+
+start_time = 1_634_515_200
+week = 7 * 24 * 3_600
+end_time = start_time + week
+counter = 0
+time_process = [0] * week
+
+for times in f:
+    start_process, end_process = map(int,times.split())
+    if start_process <= start_time and (end_process >= start_time or end_process == 0):
+        counter = counter + 1
+    
+    if start_process >= start_time and start_process <= end_time:
+        time_process[start_process - start_time] = time_process[start_process - start_time] + 1
+    
+    if end_process >= start_time and end_process <= end_time:
+        time_process[end_process - start_time] = time_process[end_process - start_time] - 1
+
+sum_time = 0
+max_process = 0
+
+for sec in range(week):
+    counter = counter + time_process[sec]
+    if counter > max_process:
+        max_process = counter
+        sum_time = 0
+    if counter == max_process:
+        sum_time = sum_time + 1
+
+print(max_process, sum_time)
