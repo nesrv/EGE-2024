94e47f7ce6fbb46d5fda67cbad455b01982fee6e
ne_srv <ne_srv@mail.ru>
2024-12-20 13:35:30 +0300

    autosavw

--- 24/2025/24-45.py
+++ 24/2025/24-45.py
@@ -3,6 +3,28 @@ ABBAAABBABBXY
 XYYYXYAB
 '''.split()
 
-for sub_s in s:
-    sub_s = sorted(sub_s, key=len)
-    print(sub_s)
+
+s = open('24/2025/24 (1).txt')
+
+# Найти самую длинную цепочку из одиннаковых символов
+max_c = 0 # сколько раз встречается буква
+current_len = 0 # длина самой длиной цепочкой с этой буквой
+c = 0
+for sub_s in s:   
+    for i in range(len(sub_s) - 1):
+        if sub_s[i] == sub_s[i + 1]:
+            c += 1
+            if c > current_len:
+                current_len = c
+                max_c = sub_s.count(sub_s[i])
+            if c == current_len:
+                max_c = max(max_c, sub_s.count(sub_s[i]))
+        else:           
+            c = 1
+
+print(max_c)
+              
+    
+
+
+
