8de05f49a83a2b082bdd5b4832f344090dd3c89e
ne_srv <ne_srv@mail.ru>
2025-02-22 00:49:54 +0300

    autosave

---
+++ 16/я-25-ф-8.py
@@ -0,0 +1,22 @@
+# 7555448
+# 7555442 + 6
+
+def f(n):
+    if n == 1:
+        return 2
+    if n > 1 and f(n - 1) < 7555444:
+        return f(n - 1) + 6
+    return f(n - 1) - 7555444
+
+
+def f2(n):
+    if n <= 1259241:
+        return 2 + 6 * (n - 1)
+    return 7555442 - 7555444 
+
+x = (7555444 - 2) / 6 + 1
+x = 1259241
+print(x)
+
+print(f2(1259241))
+print(7555444)
